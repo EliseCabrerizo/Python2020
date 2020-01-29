d28d124d9b4ee152f356450cf79937522ce5b2ec
EliseCabrerizo <elise.cabrerizo@yahoo.fr>
2020-01-29 01:32:22 +0100

    API

--- PythonAPIDevoir/PythonAPIDevoir/settings.py
+++ PythonAPIDevoir/PythonAPIDevoir/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backoffice.apps.BackofficeConfig',
 ]
 
 MIDDLEWARE = [
--- PythonAPIDevoir/PythonAPIDevoir/urls.py
+++ PythonAPIDevoir/PythonAPIDevoir/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from backoffice.views import home
-from backoffice.views import predict
+from backoffice import views
+from django.urls import include
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url(r'^$', home),
-    url('predict/', predict, name='to_predict')
+    url('prediction/', include('backoffice.urls'))
 ]
